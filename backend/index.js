41611d7840eddbd8a4b022e348efd9ffa03ced65
minhtriet2k9 <minhtrietamoled@gmail.com>
2021-12-14 11:42:04 +0700

    MyWeb

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,15 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     switch (req.url) {
         case "/styles.css":
-            res.writeHead(200, {"Content-Type": "text/css"});
-            res.write(fs.readFileSync(path.join(__dirname, '..', 'frontend','styles.css')));    
+            res.writeHead(200, { "Content-Type": "text/css" });
+            res.write(fs.readFileSync(path.join(__dirname, '..', 'frontend', 'styles.css')));
             break;
         case "/script.js":
-            res.writeHead(200, {"Content-Type": "text/javascript"});
-            res.write(fs.readFileSync(path.join(__dirname, '..', 'frontend','script.js')));    
+            res.writeHead(200, { "Content-Type": "text/javascript" });
+            res.write(fs.readFileSync(path.join(__dirname, '..', 'frontend', 'script.js')));
             break;
         default:
-            res.writeHead(200, {"Content-Type": "text/html"});
+            res.writeHead(200, { "Content-Type": "text/html" });
             res.write(fs.readFileSync(path.join(__dirname, '..', 'frontend', 'index.html')));
     }
     res.end();
@@ -23,17 +23,17 @@ const {
 const io = new Server(server);
 const fs = require('fs');
 
-if(!fs.existsSync('/chat.log')){
-    fs.writeFile(__dirname + '/chat.log',"",function (err) {
-        if (err){
+if (!fs.existsSync('/chat.log')) {
+    fs.writeFile(__dirname + '/chat.log', "", function(err) {
+        if (err) {
             console.log(err);
         }
     })
 }
 
-if(!fs.existsSync('/info.log')){
-    fs.writeFile(__dirname + '/info.log',"",function (err) {
-        if (err){
+if (!fs.existsSync('/info.log')) {
+    fs.writeFile(__dirname + '/info.log', "", function(err) {
+        if (err) {
             console.log(err);
         }
     })
@@ -52,23 +52,23 @@ if(!fs.existsSync('/info.log')){
 // console.log(chatLog);
 // io.emit('chat-log',chatLog);
 
-io.on('connection',(socket) => {
-    var time = new Date();
-    socket.on('new-user',(data) => {
+io.on('connection', (socket) => {
+    //var time = new Date();
+    socket.on('new-user', (data) => {
         //console.log('User connected at',time.getHours()+':'+time.getMinutes()+':'+time.getSeconds(),`with name: "${name}"`);
-        fs.appendFile(__dirname+"/info.log",JSON.stringify(data)+'\n',  function(err) {
-            if(err) {
+        fs.appendFile(__dirname + "/info.log", JSON.stringify(data) + '\n', function(err) {
+            if (err) {
                 console.log(err);
             }
-        }); 
+        });
     });
     socket.on('on-chat', (message) => {
         io.emit('user-chat', message);
-        fs.appendFile(__dirname+"/chat.log",JSON.stringify(message)+'\n', function(err) {
-            if(err) {
+        fs.appendFile(__dirname + "/chat.log", JSON.stringify(message) + '\n', function(err) {
+            if (err) {
                 return console.log(err);
             }
-        }); 
+        });
     });
 });
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,13 @@
 const socket = io();
 var name;
+
 function getName() {
     name = prompt("Enter your name: ");
-    if(name == null || name == ""){
+    if (name == null || name == "") {
         alert("Please enter a valid name");
         getName();
     }
-    if(name == "\""){
+    if (name == "\"") {
         alert("Invalid name");
         getName();
     }
@@ -14,83 +15,87 @@ function getName() {
 
 var time = new Date();
 getName();
-const login = {
+login = {
     name: name,
-    time: time.getDate()+'/'+time.getMonth()+'/'+time.getFullYear()+'  '+time.getHours()+':'+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()
-}
+    time: time.getDate() + '/' + time.getMonth() + '/' + time.getFullYear() + '  ' + time.getHours() + ':' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+};
 socket.emit('new-user', login);
-const chatMes = document.querySelector('#message');
-const chatForm = document.querySelector('#chatform');
-function displayAccountName(){
+chatMes = document.querySelector('#message');
+chatForm = document.querySelector('#chatform');
+
+function displayAccountName() {
     var account = document.querySelector('#title');
-    account.setAttribute('style','white-space: pre;');
+    account.setAttribute('style', 'white-space: pre;');
     account.textContent = `${name}`;
 }
 displayAccountName();
-chatForm.addEventListener('submit', (e) => { 
-    var now= new Date();
+chatForm.addEventListener('submit', (e) => {
+    var now = new Date();
     e.preventDefault();
-    fullTime = now.getDate() + '/' + now.getMonth() + '/' + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-    time = now.getHours() + ':' + now.getMinutes() ;
-    const message = chatMes.value; 
-    if(message == "\""){
+    var fullTime = now.getDate() + '/' + now.getMonth() + '/' + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+    time = now.getHours() + ':' + now.getMinutes();
+    message = chatMes.value;
+    if (message == "\"") {
         alert("You can't use \"");
         return;
     }
-    const chat = {
+    chat = {
         name: name,
         message: message,
         time: time,
         fullTime: fullTime
     };
-    socket.emit('on-chat',chat);
+    socket.emit('on-chat', chat);
     localStorage.setItem('chatHistory', JSON.stringify(chatHistory));
     chatMes.value = '';
-})
-const messages = document.querySelector('#messages');
+});
+messages = document.querySelector('#messages');
 socket.on('user-chat', (data) => {
-    if(data.message == ''){
+    if (data.message == '') {
         return;
     }
-    const chatLabel = document.createElement('i');
-    if(data.name == name){
-        chatLabel.textContent =  `Me\r\n`;
+    chatLabel = document.createElement('i');
+    if (data.name == name) {
+        chatLabel.textContent = `Me\r\n`;
         chatLabel.setAttribute('style', 'white-space: pre; font-weight: bold; font-size: 14px; color: #ff2354; padding-right: 15px; margin-top: 7px;');
-    }else{
-        chatLabel.textContent =  `${data.name}\r\n`;
+    } else {
+        chatLabel.textContent = `${data.name}\r\n`;
         chatLabel.setAttribute('style', 'white-space: pre; font-weight: bold; font-size: 14px; color: #06f; padding-left: 15px; margin-top: 7px;');
     }
-    const chatMsg = document.createElement('label');
-    if(data.name == name){
-       chatMsg.setAttribute('style', 'font-size: 20px; padding-right: 30px; white-space: pre; margin-top: 7px;');
-    }else{
+    chatMsg = document.createElement('label');
+    if (data.name == name) {
+        chatMsg.setAttribute('style', 'font-size: 20px; padding-right: 30px; white-space: pre; margin-top: 7px;');
+    } else {
         chatMsg.setAttribute('style', 'font-size: 20px; padding-left: 30px; white-space: pre; margin-top: 7px;');
     }
     chatMsg.textContent = `${data.message}\r\n`;
 
-    const chatTime = document.createElement('label');
-    if(data.name == name){
+    chatTime = document.createElement('label');
+    if (data.name == name) {
         chatTime.setAttribute('style', 'font-size: 13px; padding-right: 15px; white-space: pre; margin-top: 7px;');
-    }else{
+    } else {
         chatTime.setAttribute('style', 'font-size: 13px; padding-left: 15px; white-space: pre; margin-top: 7px;');
     }
-    chatTime.textContent = `${data.time}`;
+    chatTime.textContent = `${data.time} `;
 
-    const chatItem = document.createElement('div');
-    if(data.name == name){
-        chatItem.setAttribute('style','text-align: right; padding-bottom: 10px;');
+    chatItem = document.createElement('div');
+    if (data.name == name) {
+        chatItem.setAttribute('style', 'text-align: right; padding-bottom: 10px;');
     }
     chatItem.appendChild(chatLabel);
     chatItem.appendChild(chatMsg);
     chatItem.appendChild(chatTime);
-
     messages.appendChild(document.createElement('br'));
     messages.appendChild(chatItem);
-})
+});
 window.setInterval(function() {
     var elem = document.getElementById('messages');
     elem.scrollTop = elem.scrollHeight;
 }, 1);
-window.onscroll = function (ev) {
+window.onscroll = function(ev) {
     document.getElementById("message").focus();
+};
+
+if ('a' === 'a') {
+    alert("bruh");
 }
